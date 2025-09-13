f517a0d3395e592fa3430947e92eeb5f1b50f509
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2025-09-13 20:29:40 -0300

    Alterando padrão do teclado para QWERT

--- src/app/components/virtual-key-board/virtual-key-board.ts
+++ src/app/components/virtual-key-board/virtual-key-board.ts
@@ -1,39 +1,31 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-virtual-key-board',
-  imports: [NgFor],
+  imports: [CommonModule],
   templateUrl: './virtual-key-board.html',
   styleUrl: './virtual-key-board.css'
 })
 export class VirtualKeyBoard {
-  letras: string[] = [];
-  letrasClicadas: string[] = []; // Nova propriedade para rastrear cliques
+  // Layout QWERTY dividido por linhas
+  linhasTeclado: string[][] = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+  ];
 
   @Input() letrasTentadas: string[] = [];
   @Output() letraClicada = new EventEmitter<string>();
 
-  ngOnInit(): void {
-    this.gerarAlfabeto();
-  }
-
-  private gerarAlfabeto(): void {
-    for (let i = 0; i < 26; i++) {
-      this.letras.push(String.fromCharCode(97 + i).toUpperCase());
-    }
-  }
-
   onLetraClick(letra: string): void {
     if (!this.isLetraDesabilitada(letra)) {
-      this.letrasClicadas.push(letra); // Registra o clique localmente
       this.letraClicada.emit(letra);
     }
   }
 
   isLetraDesabilitada(letra: string): boolean {
-    letra = letra.toLocaleLowerCase();
-    return this.letrasTentadas.includes(letra)
+    const tentadas = this.letrasTentadas || [];
+    return tentadas.map(l => l.toLowerCase()).includes(letra.toLowerCase());
   }
 }
